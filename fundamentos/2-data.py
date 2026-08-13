8c6768097aa43904a61963c1d7273c6ddc247cbd
J00J3X <jonathan.krefta040@gmail.com>
2026-08-13 02:16:57 -0300

    types of data in python

---
+++ fundamentos/2-data.py
@@ -0,0 +1,14 @@
+# PythonBookLibrary
+title       = "Game Of Thrones"
+yearLaunch  = 2023
+bookRatio   = 9.5
+isInLibrary = False
+
+print(title)
+print(yearLaunch)
+print(bookRatio)
+
+print(type(title))
+print(type(yearLaunch))
+print(type(bookRatio))
+print(type(isInLibrary))
