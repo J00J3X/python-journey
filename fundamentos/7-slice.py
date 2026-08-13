48ea47d78d9b5daec4fc8597d2688d1149aaff4e
J00J3X <jonathan.krefta040@gmail.com>
2026-08-13 13:32:23 -0300

    Slice and String methods

---
+++ fundamentos/7-slice.py
@@ -0,0 +1,20 @@
+bookTitle = "Game of Thrones"
+# string[begin:final] - index start in 0 | final index - 1
+
+# 1 - Search all the string by the first position
+print(bookTitle[0:])
+
+# 2 - Search all the string until the last position
+print(bookTitle[:16])
+
+# 3 - Search all the string by the third until the last position
+print(bookTitle[2:])
+
+# 4 - Search all the string 2 by 2 
+print(bookTitle[::2])
+
+# 5 - Search all the string by the odd index
+print(bookTitle[1::2])
+
+# 6 - Reverse the string
+print(bookTitle[::-1])
---
+++ fundamentos/8-string_methods.py
@@ -0,0 +1,14 @@
+bookTitle = "Game of Thrones"
+bookDescription = """
+    Game of Thrones is a best-seller romance with
+    fantasy, action and dragons!!
+    """
+
+print(bookTitle.upper())
+print(bookTitle.lower())
+print(bookTitle.capitalize())
+print(bookTitle.title())
+print(bookTitle.center(6, '-')) 
+print(bookTitle.find("h"))
+print(bookTitle.replace("Game", "Play"))
+print(bookDescription.split(','))
