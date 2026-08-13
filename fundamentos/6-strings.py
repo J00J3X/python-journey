aaddd49680cfe4597df0215a3dceaabe684ad1fc
J00J3X <jonathan.krefta040@gmail.com>
2026-08-13 13:12:11 -0300

    Strings in Python

---
+++ fundamentos/6-strings.py
@@ -0,0 +1,18 @@
+bookTitle   = "Game of Thrones"
+bookTitle2  = "game of Thrones"
+print(bookTitle == bookTitle2) # Case Sensitive
+
+bookDescription = """
+    Game of Thrones is a best-seller romance with
+    fantasy, action and dragons!!
+    """
+
+print(bookTitle)
+# 1- Multiplying strings
+line = "="
+print(line*30)
+print(bookDescription)
+
+# 2- Searching a word inside the text
+print("game" in bookTitle)
+print("Action" in bookTitle)
